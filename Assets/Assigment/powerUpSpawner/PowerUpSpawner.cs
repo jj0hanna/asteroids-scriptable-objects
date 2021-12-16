295a16785d7b6f839c5bafdc1afc31343729762b
Johanna Johansson <johannajohansson642@gmail.com>
2021-12-16 15:04:18 +0100

    started

---
+++ Assets/Assigment/powerUpSpawner/PowerUpSpawner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Assigment.powerUpSpawner
+{
+    public class PowerUpSpawner : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
